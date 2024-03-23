67e76b98eb79e292b8824b0726658773ee8db1d9
Ryan Mason <ryan.mason814@gmail.com>
2024-03-22 23:50:13 -0700

    added treesitter stuff

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    -- only load treesitter for a existing buffer or a new buffer.
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     config = function ()
         local configs = require("nvim-treesitter.configs")
 
