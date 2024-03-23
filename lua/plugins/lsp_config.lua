6cae1d96d2f081cae53feea163cba80a964530fa
Ryan Mason <ryan.mason814@gmail.com>
2024-03-23 00:06:37 -0700

    added global keybinds!

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -170,6 +170,16 @@ return {
             lspconfig.zls.setup({
                 capabilities = capabilities,
             })
+
+            -- global kepmaps
+            vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
         end,
     },
 
