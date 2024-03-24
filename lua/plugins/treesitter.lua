6bd1dc514043f30cbc96c7bc208c47b4496ac29b
JoshBenn <sublimeibanez@protonmail.com>
2024-03-24 14:51:42 -0700

    Working through which-key

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -32,28 +32,28 @@ return {
             })
         end
     },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {},
-        config = function()
-            local inlay_highlight = { "inlay", }
-            local scope_highlight = { "scope", }
-
-            local hooks = require "ibl.hooks"
-            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-                vim.api.nvim_set_hl(0, "inlay", { fg = "#7f623d"})
-                vim.api.nvim_set_hl(0, "scope", { fg = "#777777"})
-            end)
-            require("ibl").setup({
-                indent = {
-                    highlight = inlay_highlight
-                },
-                scope = {
-                    highlight = scope_highlight
-                }
-            })
-        end,
-    },
+    -- {
+    --     "lukas-reineke/indent-blankline.nvim",
+    --     main = "ibl",
+    --     opts = {},
+    --     config = function()
+    --         local inlay_highlight = { "inlay", }
+    --         local scope_highlight = { "scope", }
+    --
+    --         local hooks = require "ibl.hooks"
+    --         hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    --             vim.api.nvim_set_hl(0, "inlay", { fg = "#7f623d"})
+    --             vim.api.nvim_set_hl(0, "scope", { fg = "#777777"})
+    --         end)
+    --         require("ibl").setup({
+    --             indent = {
+    --                 highlight = inlay_highlight
+    --             },
+    --             scope = {
+    --                 highlight = scope_highlight
+    --             }
+    --         })
+    --     end,
+    -- },
 }
 
