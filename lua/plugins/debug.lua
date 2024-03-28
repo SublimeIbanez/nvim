93339b77a5224a4d8793606ae9286ca7de100825
JoshBenn <sublimeibanez@protonmail.com>
2024-03-28 03:37:08 -0700

    Got go debug working

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -5,10 +5,13 @@ return {
         dependencies = {
             "rcarriga/nvim-dap-ui",
             "nvim-neotest/nvim-nio",
+            "leoluz/nvim-dap-go",
         },
         config = function()
             local dap = require("dap")
             local dapui = require("dapui")
+            require("dap-go").setup()
+            dapui.setup()
 
             dap.listeners.before.attach.dapui_config = function()
                 dapui.open()
@@ -23,6 +26,93 @@ return {
                 dapui.close()
             end
 
+            -- Adapter / Config
+            -- Golang
+            dap.adapters.delve = {
+                type = 'server',
+                port = '${port}',
+                executable = {
+                    command = 'dlv',
+                    args = { 'dap', '-l', '127.0.0.1:${port}' },
+                }
+            }
+
+            -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+            dap.configurations.go = {
+                {
+                    type = "delve",
+                    name = "Debug",
+                    request = "launch",
+                    program = "${file}"
+                },
+                {
+                    type = "delve",
+                    name = "Debug test", -- configuration for debugging test files
+                    request = "launch",
+                    mode = "test",
+                    program = "${file}"
+                },
+                -- works with go.mod packages and sub packages
+                {
+                    type = "delve",
+                    name = "Debug test (go.mod)",
+                    request = "launch",
+                    mode = "test",
+                    program = "./${relativeFileDirname}"
+                }
+            }
+
+            -- C / C++ / Rust
+            dap.adapters.cppdbg = {
+                id = "cppdbg",
+                type = "executable",
+                command = "/home/kokurio/.local/share/nvim/mason/packages/cpptools/extensions/debugAdapters/bin/OpenDebugAD7", -- Must be absolute path
+            }
+            dap.configurations.cpp = {
+                {
+                    name = "Launch",
+                    type = "cppdbg",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopAtEntry = true,
+                    setupCommands = {
+                        {
+                            text = '-enable-pretty-printing',
+                            description = 'enable pretty printing',
+                            ignoreFailures = false
+                        },
+                    },
+
+                },
+                {
+                    name = 'Attach to gdbserver :1234',
+                    type = 'cppdbg',
+                    request = 'launch',
+                    MIMode = 'gdb',
+                    miDebuggerServerAddress = 'localhost:1234',
+                    miDebuggerPath = '/usr/bin/gdb',
+                    cwd = '${workspaceFolder}',
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    setupCommands = {
+                        {
+                            text = '-enable-pretty-printing',
+                            description = 'enable pretty printing',
+                            ignoreFailures = false
+                        },
+                    },
+
+                },
+            }
+            dap.configurations.c = dap.configurations.cpp
+            dap.configurations.rust = dap.configurations.cpp
+
+
+            -- Keymap
             vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint,
                 { noremap = true, silent = true, desc = "Toggle Breakpoint" })
             vim.keymap.set("n", "<leader>dc", dap.continue,
--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -60,7 +60,7 @@ return {
                 window = {
                     border = "none",            -- none, single, double, shadow
                     position = "bottom",        -- bottom, top
-                    margin = { 1, 35, 1, 0.55 }, -- [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
+                    margin = { 1, 35, 1, 0.55 }, -- [top, right, bottom, left]. Between 0 and 1 will be treated as a percentage
                     padding = { 1, 2, 1, 2 },   -- extra window padding [top, right, bottom, left]
                     winblend = 20,              -- value between 0-100 0 for fully opaque and 100 for fully transparent
                     zindex = 1000,              -- positive value to position WhichKey above other floating windows.
