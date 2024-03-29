9a0815f142f82ef72404bf47c745152ff2b2589a
JoshBenn <sublimeibanez@protonmail.com>
2024-03-29 08:21:07 -0700

    Added some baseline quick fixes for startup if desired

--- lua/config.lua
+++ lua/config.lua
@@ -1,9 +1,4 @@
 -- Base configuration
--- vim.cmd("set termguicolors")
--- vim.cmd("set tabstop=4")
--- vim.cmd("set softtabstop=4")
--- vim.cmd("set shiftwidth=4")
--- vim.cmd("set signcolumn=yes")
 vim.opt.termguicolors = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -16,5 +11,5 @@ vim.o.scrolloff = 5
 vim.o.cursorline = true
 vim.opt.autoindent = true
 
-vim.cmd("set number")
+vim.cmd("set relativenumber")
 vim.opt.listchars = { space = "·", eol = "⏎", tab = "»▸", }
